29a331cf6f1ea39ada438f7e89552365c1143cdc
faisalmch <ummyahya765@gmail.com>
2025-02-03 20:52:13 +0400

    updated auth-service and db.js files

--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('../../../shared/db');
 const authRoutes = require('./routes/authRoutes');
-require('dotenv').config({ path: '../../../.env' });
+// require('dotenv').config({ path: '../../../.env' });
+const dotenv = require('dotenv');
+const path = require('path');
+const envPath = path.resolve(__dirname, '../../../.env');  // Ensure correct path
+console.log("Loading .env from:", envPath);  // Debugging output
+dotenv.config({ path: envPath });
 
 const app = express();
 
 // Middleware
 app.use(express.json()); // This allows us to parse JSON bodies in requests
 
+console.log("MONGO_URI from .env:", process.env.MONGO_URI);  // Debugging line
 // Connect to MongoDB
 connectDB();
 
